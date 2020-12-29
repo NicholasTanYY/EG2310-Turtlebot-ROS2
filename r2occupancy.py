68b4669b2794fc8533462b300751bf2a27a4192d
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-12-29 23:00:19 +0800

    Updated for ROS2.

---
+++ r2occupancy.py
@@ -0,0 +1,62 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
+
+# constants
+occ_bins = [-1, 0, 100, 101]
+
+
+class Occupy(Node):
+
+    def __init__(self):
+        super().__init__('occupy')
+        self.subscription = self.create_subscription(
+            OccupancyGrid,
+            'map',
+            self.listener_callback,
+            qos_profile_sensor_data)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        # create numpy array
+        occdata = np.array([msg.data])
+		# compute histogram to identify percent of bins with -1
+		occ_counts = np.histogram(occdata,occ_bins)
+		# calculate total number of bins
+		total_bins = msg.info.width * msg.info.height
+		# log the info
+        self.get_logger().info('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i' % (occ_counts[0][0], occ_counts[0][1], occ_counts[0][2], total_bins))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    occupy = Occupy()
+
+    rclpy.spin(occupy)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    occupy.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
