0edd04461d50480bf71326603bbb6939d3d3bb7e
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-12-29 11:03:45 +0800

    Added config files necessary for colcon.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'auto_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nus',
+    maintainer_email='nus@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'r2mover = auto_nav.r2mover:main',
+            'r2moverotate = auto_nav.r2moverotate:main',
+        ],
+    },
+)
