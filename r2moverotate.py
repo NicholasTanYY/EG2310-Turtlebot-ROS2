b6392acbad21df39737e408b7a4ea607fbb6b6f9
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-12-29 10:47:08 +0800

    Updated to use ROS2. Had to add spin_once to get everything to work.

---
+++ r2moverotate.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# adapted from https://github.com/Shashika007/teleop_twist_keyboard_ros2/blob/foxy/teleop_twist_keyboard_trio/teleop_keyboard.py
+
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+import math
+import cmath
+import numpy as np
+
+# constants
+rotate_speed = 0.1
+linear_speed = 0.1
+
+
+# code from https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z # in radians
+
+
+# function to check if keyboard input is a number as
+# isnumeric does not handle negative numbers
+def isnumber(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+# class for moving and rotating robot
+class Mover(Node):
+    def __init__(self):
+        super().__init__('moverotate')
+        self.publisher_ = self.create_publisher(Twist,'cmd_vel',10)
+        # self.get_logger().info('Created publisher')
+        self.subscription = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            10)
+        # self.get_logger().info('Created subscriber')
+        self.subscription  # prevent unused variable warning
+        # initialize variables
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+
+
+    # function to set the class variables using the odometry information
+    def odom_callback(self, msg):
+        # self.get_logger().info(msg)
+        # self.get_logger().info('In odom_callback')
+        orientation_quat =  msg.pose.pose.orientation
+        self.roll, self.pitch, self.yaw = euler_from_quaternion(orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w)
+
+
+    # function to rotate the TurtleBot
+    def rotatebot(self, rot_angle):
+        # self.get_logger().info('In rotatebot')
+        # create Twist object
+        twist = Twist()
+        
+        # get current yaw angle
+        current_yaw = self.yaw
+        # log the info
+        self.get_logger().info('Current: %f' % math.degrees(current_yaw))
+        # we are going to use complex numbers to avoid problems when the angles go from
+        # 360 to 0, or from -180 to 180
+        c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
+        # calculate desired yaw
+        target_yaw = current_yaw + math.radians(rot_angle)
+        # convert to complex notation
+        c_target_yaw = complex(math.cos(target_yaw),math.sin(target_yaw))
+        self.get_logger().info('Desired: %f' % math.degrees(cmath.phase(c_target_yaw)))
+        # divide the two complex numbers to get the change in direction
+        c_change = c_target_yaw / c_yaw
+        # get the sign of the imaginary component to figure out which way we have to turn
+        c_change_dir = np.sign(c_change.imag)
+        # set linear speed to zero so the TurtleBot rotates on the spot
+        twist.linear.x = 0.0
+        # set the direction to rotate
+        twist.angular.z = c_change_dir * rotate_speed
+        # start rotation
+        self.publisher_.publish(twist)
+
+        # we will use the c_dir_diff variable to see if we can stop rotating
+        c_dir_diff = c_change_dir
+        # self.get_logger().info('c_change_dir: %f c_dir_diff: %f' % (c_change_dir, c_dir_diff))
+        # if the rotation direction was 1.0, then we will want to stop when the c_dir_diff
+        # becomes -1.0, and vice versa
+        while(c_change_dir * c_dir_diff > 0):
+            # allow the callback functions to run
+            rclpy.spin_once(self)
+            current_yaw = self.yaw
+            # convert the current yaw to complex form
+            c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
+            self.get_logger().info('Current Yaw: %f' % math.degrees(current_yaw))
+            # get difference in angle between current and target
+            c_change = c_target_yaw / c_yaw
+            # get the sign to see if we can stop
+            c_dir_diff = np.sign(c_change.imag)
+            # self.get_logger().info('c_change_dir: %f c_dir_diff: %f' % (c_change_dir, c_dir_diff))
+
+        self.get_logger().info('End Yaw: %f' % math.degrees(current_yaw))
+        # set the rotation speed to 0
+        twist.angular.z = 0.0
+        # stop the rotation
+        self.publisher_.publish(twist)
+
+# function to read keyboard input
+    def readKey(self):
+        twist = Twist()
+        try:
+            while True:
+                # get keyboard input
+                cmd_char = str(input("Keys w/x/a/d -/+int s: "))
+        
+                # use our own function isnumber as isnumeric 
+                # does not handle negative numbers
+                if isnumber(cmd_char):
+                    # rotate by specified angle
+                    self.rotatebot(int(cmd_char))
+                else:
+                    # check which key was entered
+                    if cmd_char == 's':
+                        # stop moving
+                        twist.linear.x = 0.0
+                        twist.angular.z = 0.0
+                    elif cmd_char == 'w':
+                        # move forward
+                        twist.linear.x = 0.1
+                        twist.angular.z = 0.0
+                    elif cmd_char == 'x':
+                        # move backward
+                        twist.linear.x = -0.1
+                        twist.angular.z = 0.0
+                    elif cmd_char == 'a':
+                        # turn counter-clockwise
+                        twist.linear.x = 0.0
+                        twist.angular.z = 1.0
+                    elif cmd_char == 'd':
+                        # turn clockwise
+                        twist.linear.x = 0.0
+                        twist.angular.z = -1.0
+                        
+                    # start the movement
+                    self.publisher_.publish(twist)
+    
+        except Exception as e:
+            print(e)
+            
+		# Ctrl-c detected
+        finally:
+        	# stop moving
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            self.publisher_.publish(twist)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    mover = Mover()    
+    mover.readKey()
+    
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    mover.destroy_node()
+    
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
