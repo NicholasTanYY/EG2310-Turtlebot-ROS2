3b2809f3f80bc473f379a3e591906726f9393d3e
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-12-29 21:52:13 +0800

    Updated to use ROS2. Had to add qos_profile_sensor_data to create_subscription command.

---
+++ r2scanner.py
@@ -0,0 +1,60 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from sensor_msgs.msg import LaserScan
+import numpy as np
+
+
+class Scanner(Node):
+
+    def __init__(self):
+        super().__init__('scanner')
+        self.subscription = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.listener_callback,
+            qos_profile_sensor_data)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        # create numpy array
+        laser_range = np.array(msg.ranges)
+        # replace 0's with nan
+        laser_range[laser_range==0] = np.nan
+        # find index with minimum value
+        lr2i = np.nanargmin(laser_range)
+
+        # log the info
+        self.get_logger().info('Shortest distance at %i degrees' % lr2i)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    scanner = Scanner()
+
+    rclpy.spin(scanner)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    scanner.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
